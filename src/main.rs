88772fdc7bcc070125f1a66891fe57bbea1df694
zynapse <zynapse@zynapse.cortex>
2017-11-09 19:16:02 +0100

    More comprehensive example

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ use rusty_bike::network::Network;
 fn main() {
     println!("Hello World!");
 
-    let net = Network::with_shape(&[3, 128, 412, 10]);
-    let result = net.feed_forward(&[1.0, 0.5, 1.25]);
+    let net = Network::with_shape(&[3, 3, 2]);
+    let result = net.feed_forward(&[1.0, 2.0, 3.0]);
     
     println!("Result: {:?}", result);
 }
